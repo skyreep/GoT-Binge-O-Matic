e7e28a44292f129a35a721dbac5c97cb247182d9
Skyreep <skyreep@gmail.com>
2019-04-10 11:40:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-//Set GoT Runtime for all episodes in minutes
-var runtime = 3810;
-
-// Set the date we're counting down to
+// Set the date of the GoT Season 8 Premiere
 var countDownDate = new Date("Apr 14, 2019 21:00:00").getTime();
 
-// Update the count down every 1 second
+// Update the count down stats every 1 second
 var x = setInterval(function() {
 
-  // Get todays date and time
+  // Get the current date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
+  // Find the distance between the current time and the premiere time
   var distance = countDownDate - now;
 
-  // Time calculations for remaining days, hours, minutes and seconds
+  // Calculate the days, hours, minutes and seconds until the premiere by converting the from ms
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -25,24 +22,31 @@ var x = setInterval(function() {
   // Calculate the percent of remaining time required to successfully watch all episodes
   var percentTimeReq = (runtime / distance);
 
-  // Display the countdown
+  // Display the countdown to the premiere in the DOM
   document.getElementById("GOTCountdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-  // If it's still possible to binge every episode before the premiere
+  // If it's still possible to binge every episode before the premiere do the following
   if (percentTimeReq < 1) {
     // Display the percentage of your remaining time until premiere you'd have to spend bingeing
-    document.getElementById("BingePercent").innerHTML = (percentTimeReq * 100).toFixed(2) + "% of your total remaining time until the premier will need to be spent binging.";
+    document.getElementById("BingePercent").innerHTML = (percentTimeReq * 100).toFixed(2) + "% of your total remaining time until the premiere will need to be spent binging.";
 
     // Calculate hours per day required for said binge
     var bingeHoursPerDay = ((days * 24) * percentTimeReq) / days;
 
     // Display the daily binge countdown
     document.getElementById("HoursPerDay").innerHTML = "You need to spend " + bingeHoursPerDay.toFixed(1) + " hours per day binging Game of Thrones for the next " + days + " days starting tomorrow.";
+  } else {
+    // Display a message if it's impossible to binge
+    document.getElementById("BingePercent").innerHTML = "You're too late to binge every episode beforehand!";
+    
+    // Display another message in lieu of the "binge hours per day" section if it is impossible
+    document.getElementById("HoursPerDay").innerHTML = "You're too late to binge every episode beforehand!";
   }
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("GOTCountdown").innerHTML = "EXPIRED";
+    document.getElementById("GOTCountdown").innerHTML = "Winter is here!!!";
   }
 }, 1000); //setInterval to update every second
+
